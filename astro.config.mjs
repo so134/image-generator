411459243147c3bdcd91ca90d99aaca7c946a757
so134 <i.sou010101@gmail.com>
2025-12-25 13:33:31 +0900

    feat: sync local project with remote

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [react()],
+  server: {
+    port: 4321,
+    host: true
+  }
+});
